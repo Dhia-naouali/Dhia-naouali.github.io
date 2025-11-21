8f571bcc4ccd0c7dacc218d2a702d8f8120571c8
Dhia-naouali <dhia12naouali@gmail.com>
2025-11-21 18:04:02 +0100

    blogs & notes

---
+++ blogs_notes/adjust.bash
@@ -0,0 +1,10 @@
+for f in */index.html; do
+  perl -i -pe '
+    if (m|</head>|) {
+      $_ = q|<style>
+.notion-topbar { display: none !important; }
+.notion-frame { padding-left: 3rem !important; }
+</style>| . $_;
+    }
+  ' "$f"
+done
